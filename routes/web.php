f1a30026af8550cb622654bd1583c6b9487f00e2
Alfahan <ali.farhan160@gmail.com>
2023-02-22 10:50:28 +0700

    Add Permission

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,9 @@ Route::prefix('apps')->group(function() {
     Route::group(['middleware' => ['auth']], function () {
         // route dashboard
         Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+
+        // route permissions
+        Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')->middleware('permission:permissions.index');
     });
 });
 
